503c03b4a7bac5b67a9fc0c66776d3aab59b9581
Gannis <iannisballas@yahoo.gr>
2018-10-04 17:46:24 +0300

    Changes before section 15

---
+++ gulp/tasks/scripts.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+webpack = require('webpack')
+
+
+gulp.task('scripts', function(callback){
+	webpack(require('../../webpack.config.js'), function(){
+	console.log('Hooray, webpack completed!');
+	callback();
+	});
+});
